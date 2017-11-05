2d63ab1e47ed29740b4256c87a30c0dc7d289571
Simon Berger <siku2@outlook.de>
2017-11-06 00:41:29 +0100

    CORS!

--- GiTils/gitils.py
+++ GiTils/gitils.py
@@ -1,8 +1,11 @@
 import flask
+import flask_cors
 
 from . import constants
 from .blueprints import token_provider
 
 app = flask.Flask("GiTils")
+
+flask_cors.CORS(app)
 
 app.register_blueprint(token_provider.blueprint)
