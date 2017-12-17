2e21329e30b945f2f6a596d70f05097ad0c55f03
Simon Berger <siku2@outlook.de>
2017-12-17 23:26:54 +0100

    there we go. caching is left

--- GiTils/blueprints/lyrics.py
+++ GiTils/blueprints/lyrics.py
@@ -1,8 +1,9 @@
 """Lyrics endpoint."""
 
 from flask import Blueprint, jsonify
+from lyrics import search_lyrics
 
-from ..lyrics import search_for_lyrics
+from ..constants import Google
 
 blueprint = Blueprint("lyrics", "GiTils")
 
@@ -10,5 +11,6 @@ blueprint = Blueprint("lyrics", "GiTils")
 @blueprint.route("/lyrics/<query>")
 def get_lyrics(query):
     """Return lyrics for query."""
-    lyrics = search_for_lyrics(query)
-    return jsonify(lyrics)
+    lyrics_data = search_lyrics(query, google_api_key=Google.API_KEY)
+
+    return jsonify(lyrics_data.to_dict())
